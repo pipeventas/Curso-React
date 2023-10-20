de071db98a08a8e90f2e81308acf863544506993
Felipe Jaramillo <lfja333@gmail.com>
2023-10-20 01:11:37 -0500

    Firts Commit

---
+++ src/Components/FirtsComponent.js
@@ -0,0 +1,21 @@
+
+
+import React from 'react';
+
+export const FirtsComponent = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>This is my Firts Component</h1>
+        <p>It's a text of my firts component </p>
+
+        <a href="https://lnkd.in/d3qY4mmw" target="_blank">Explaining 8 Popular Network Protocols</a>
+
+        <a href="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F6f9e43fa-84d5-4875-817c-c2e1af75d16e_1280x1664.gif" target="_blank">Eight network protocols explained in a single diagram.</a>
+
+        <a href="https://adasas123-my.sharepoint.com/personal/adasas_adasas123_onmicrosoft_com/_layouts/15/onedrive.aspx?login_hint=adasas%40adasas123%2Eonmicrosoft%2Ecom&view=15" target="_blank">Eight network protocols explained in a single diagram.</a>
+         </header>
+    </div>
+  )
+}
